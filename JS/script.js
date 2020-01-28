b9b6a687e16e3f35557fb3cd465d2fe8d2eebf04
emu <bsse0927@iit.du.ac.bd>
2020-01-28 15:24:34 +0600

    upto get-api-data

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,6 @@
 document.getElementById('getText').addEventListener('click', getText);
 document.getElementById('getUsers').addEventListener('click', getUsers);
+document.getElementById('getPosts').addEventListener('click', getPosts);
 
 function getText()  {
 //   console.log(12);
@@ -29,6 +30,25 @@ function getUsers() {
             <li> Name: ${user.name} </li>
             <li> Email: ${user.email}</li>
             </ul>
+            `;
+        });
+        document.getElementById('output').innerHTML = output;
+    })
+}
+
+function getPosts() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res=>res.json())
+    .then(data=>    {
+        let output='<h2>Posts<h2>';
+        data.forEach(function(post) {
+            output += `
+            <div>
+
+            <h3>${post.title} </h3>
+            <p> ${post.body}</p>
+
+            </div>
             `;
         });
         document.getElementById('output').innerHTML = output;
