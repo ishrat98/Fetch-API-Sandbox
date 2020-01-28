e90fec2fc9fa62a4768dffb3b320fe0a93a12758
emu <bsse0927@iit.du.ac.bd>
2020-01-28 13:10:00 +0600

    upto get-text

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,16 @@
 document.getElementById('getText').addEventListener('click', getText);
 
 function getText()  {
-  console.log(12);
+//   console.log(12);
+
+fetch('text.txt')
+.then(res=> {
+   return res.text();
+})
+.then(data=>    {
+    console.log(data);
+})
+
   }
 
 
